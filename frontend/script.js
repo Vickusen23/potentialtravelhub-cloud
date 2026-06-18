94bf65628317d33cccc33d50fc3d3a1a90891c33
Victor Pius Usen <vickusen1@gmail.com>
2026-06-18 20:33:09 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,36 @@
-document.getElementById("inquiryForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
+const form = document.getElementById("inquiryForm");
 
-    alert("Form submission will be connected to API Gateway soon.");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    fullName: document.getElementById("fullName").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    service: document.getElementById("service").value,
+    message: document.getElementById("message").value
+  };
+
+  try {
+    const response = await fetch(
+      "https://p30rp2pbrc.execute-api.us-east-1.amazonaws.com/inquiry",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+    );
+
+    const result = await response.json();
+
+    alert(result.message || "Inquiry submitted successfully!");
+
+    form.reset();
+
+  } catch (error) {
+    console.error(error);
+    alert("Something went wrong. Please try again.");
+  }
 });
